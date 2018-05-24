693e83e61c6a6a83ba81c03f85aead8cdcb1c6e9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the ASP.NET MVC Query Builder to provide data to the GridView extension
+
+
+This example demonstrates how to add the Query Builder control to an ASP.NET MVC application and use queries to supply data to the <a href="https://documentation.devexpress.com/AspNet/8998/ASP-NET-MVC-Extensions/Grid-View/Overview/Overview-GridView">ASP.NET MVC GridView</a>.<br><br>Refer to the <a href="https://documentation.devexpress.com/AspNet/120171/ASP-NET-MVC-Extensions/Query-Builder/Getting-Started">Query Builder - Getting Started</a> documentation section to learn how to accomplish this task step by step.
+
+<br/>
+
+
---
+++ VB/MvcQueryBuilder/Controllers/QueryBuilderController.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Web.Mvc
+Imports DevExpress.Web.Mvc
+
+Namespace MvcQueryBuilder.Controllers
+    ' Processes Query Builder requests.
+    Public Class QueryBuilderController
+        Inherits DevExpress.Web.Mvc.Controllers.QueryBuilderApiController
+
+        Public Overrides Function Invoke() As ActionResult
+            Return MyBase.Invoke()
+        End Function
+
+        Public Function Save() As ActionResult
+            Dim result = QueryBuilderExtension.GetSaveCallbackResult("QueryBuilder")
+            Session("SelectCommand") = result.SelectStatement
+            Return RedirectToAction("Index", "Home")
+        End Function
+    End Class
+End Namespace
