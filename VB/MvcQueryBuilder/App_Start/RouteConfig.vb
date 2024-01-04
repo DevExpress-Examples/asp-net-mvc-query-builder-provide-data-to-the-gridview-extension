35f2c560964b899bc74c7ea0d92d9c4f6c41d22d
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2024-01-04 15:08:38 +0400

    auto sync for version 2023.2

--- VB/MvcQueryBuilder/App_Start/RouteConfig.vb
+++ VB/MvcQueryBuilder/App_Start/RouteConfig.vb
@@ -1,17 +1,17 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web
 Imports System.Web.Mvc
 Imports System.Web.Routing
 
 Namespace MvcQueryBuilder
-	Public Class RouteConfig
-		Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
-			routes.IgnoreRoute("{resource}.ashx/{*pathInfo}")
+    Public Class RouteConfig
+        Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+            routes.IgnoreRoute("{resource}.ashx/{*pathInfo}")
 
-			routes.MapRoute(name:= "Default", url:= "{controller}/{action}/{id}", defaults:= New With {Key .controller = "Home", Key .action = "Index", Key .id = UrlParameter.Optional}) ' Parameter defaults -  URL with parameters -  Route name
-		End Sub
-	End Class
+            routes.MapRoute(name:="Default", url:="{controller}/{action}/{id}", defaults:=New With {
+                Key .controller = "Home",
+                Key .action = "Index",
+                Key .id = UrlParameter.Optional
+            })
+        End Sub
+    End Class
 End Namespace
